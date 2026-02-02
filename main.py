0b0be08a5a911614247d8486b508f0b1d613bc80
vaibhavagarwal46 <vaibhavagarwal478@gmail.com>
2026-02-02 21:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
-computer = random.choice([-1, 1, 0])
 
-youchoice = input("Enter your choice (SNAKE, WATER or GUN): ").upper()
+print("Type SNAKE, WATER, or GUN")
+print("Type Q to quit the game\n")
 
 youDICT = {
     "SNAKE": 1,
@@ -12,20 +12,30 @@ youDICT = {
 
 reverseDICT = {1: "SNAKE", -1: "WATER", 0: "GUN"}
 
-if youchoice not in youDICT:
-    print("Invalid choice! Please choose SNAKE, WATER, or GUN.")
-else:
+while True:
+    computer = random.choice([-1, 1, 0])
+
+    youchoice = input("Enter your choice: ").upper()
+
+    if youchoice == "Q":
+        print("Thanks for playing")
+        break
+
+    if youchoice not in youDICT:
+        print("Invalid choice... Please enter SNAKE, WATER, or GUN.")
+        continue
+
     you = youDICT[youchoice]
 
     print(f"You chose {reverseDICT[you]}")
     print(f"Computer chose {reverseDICT[computer]}")
 
     if computer == you:
-        print("It's a draw")
+        print("It's a draw!\n")
     else:
         if (computer == -1 and you == 1) or \
            (computer == 1 and you == 0) or \
            (computer == 0 and you == -1):
-            print("You win")
+            print(" You win!\n")
         else:
-            print("You lose")
+            print(" You lose!\n")
